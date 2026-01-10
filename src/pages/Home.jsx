6359e9f21c0cea08252fade632b1b93c0ac93508
MuhammadHassaan14 <nomanhassaan113@gmail.com>
2026-01-10 12:40:37 +0500

    feat: completed home page with plans and footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,8 @@ import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import AITools from '../components/AITools'
 import Testimonial from '../components/Testimonial'
-
+import Plan from '../components/Plans'
+import Footer from '../components/Footer'
 const Home = () => {
   return (
     <>
@@ -11,6 +12,8 @@ const Home = () => {
       <Hero />
       <AITools/>
       <Testimonial/>
+      <Plan/>
+      <Footer/>
     </>
   )
 }
