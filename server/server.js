996de5ca71905665fbdb947a5f46df4635da1027
MuhammadHassaan14 <nomanhassaan113@gmail.com>
2026-01-21 03:25:09 +0500

    feat): add user controller and user routes

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import { clerkMiddleware, requireAuth } from '@clerk/express'
 import aiRouter from './routes/aiRoutes.js';
 import connectCloudinary from './configs/cloudinary.js';
+import userRouter from './routes/userRoutes.js';
 
 const app = express();//initialize express app
 await connectCloudinary()
@@ -16,6 +17,7 @@ app.get('/', (req, res)=>res.send('Server is livee'));//basic route to test serv
 app.use(requireAuth());
 
 app.use('/api/ai', aiRouter)
+app.use('/api/user', userRouter)
 
 const PORT = process.env.PORT || 3000;//set port to start backend server from environment variable or default to 3000
 
